44c359d52f6b610362abe9a22cc8f0abfbc6d46c
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-13 12:15:38 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 var img = document.getElementById("madi");
+
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+
 img.onclick = function() {
-    img.style.marginLeft = '150px';
+    var interval = setInterval(moveRight, 100);
 } 
