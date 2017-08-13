4938e579e9948ae444039aebeac4de47b2bb5601
sandeepgv1 <sandeepgv1@gmail.com>
2017-08-13 12:11:09 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 var img = document.getElementById("madi");
 img.onclick = function() {
-    img.style.marginleft = '150px';
+    img.style.marginLeft = '150px';
 } 
