6d86b122c4de9323210d9bd1d7cf686edc783842
sandeepgv1 <sandeepgv1@gmail.com>
2017-10-21 16:36:27 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,19 @@ function createTemplate(data){
     return htmlTemplate;
 }
 
+function hash (input,salt) {
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req,res){
+   var hashedString = hash(req.params.input,'this-is-a-random-string');
+   res.send(hashedString);
+    
+});
+
+
+
 var pool = new Pool(config);
 
 app.get('/test-db', function(req, res){
